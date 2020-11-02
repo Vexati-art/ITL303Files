0a541e27933bffdc61a35b260148f8f7a6f7f198
Vexati-art <55270612+Vexati-art@users.noreply.github.com>
2020-11-02 18:21:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,70 +5,16 @@
   </head>
   <body>
   <h2>Downnemil</h2>
+  <nav>
+    <a href="#">HOME</a>
+    <a href="Works.html">Schoolworks</a>
+    <a href="#">About</a>
+    <div id="indicator"></div>
+  </nav>
+  
+  <a href="https://www.youtube.com/channel/UC7hSS_eujjZOEQrjsda76gA/videos" target="_blank" id="ytd-url">My YouTube Channel</a>
 
-<nav>
-	<a href="#">Home</a>
-	<a href="Works.html">Schoolworks</a>
-	<a href="#">About</a>
-	<a href="#">Contact</a>
-	<div class="animation start-home"></div>
-</nav>
-
-
-<div class="body1">
-  <div class="box">
-    <h1>UPDATES</h1>
-     
-  </div>
-  <div class="box2">
-    <h1>LATEST</h1>
+  
    
   
-</div>
 
-
-<div class="body">
-
-<div class="center">
-  <div class="property-card">
-    <a href="#">
-      <div class="property-image">
-        <div class="property-image-title">
-         
-         
-        </div>
-      </div></a>
-    <div class="property-description">
-      <h5> Basta IT, tanan may paagi.</h5
-     
-    </div>
-    <a href="#">
-      <div class="property-social-icons">
-      
-      </div>
-    </a>
-  </div>
-</div>
-</div>
-
-<div class="wrapper">
-<div class="footer">
-  <ul>
-    <li><a href="https://usls.instructure.com/login/canvas"><img src="Images/canvas.png" alt="Canvas logo" style="width:30px;height:30px;"></a></li>
-    
-    <li><a href="https://m.facebook.com/emilmrivera"> <img src="Images/facebook.png" alt="Fb logo" style="width:30px;height:30px;">
-     </a></li>
-    
-    <li><a href="http://m.gmail.com"><img src="Images/gmail.png" alt="Gmail logo" style="width:30px;height:30px;"></a></li>
-    
-    <li><a href="https://github.com/login"><img src="Images/github.png" alt="Github logo" style="width:30px;height:30px;"></a></li>
-    <li><a href="https://www.heroku.com/#"><img src="Images/heruko.png" alt="Heruko logo" style="width:30px;height:30px;"></a></li>
-      
-  </ul>
-</div>
-
-
-  
-</div>
-
-</html>
