a4058b4c4957c7dbd45514d71409ce2222b6e79f
Vexati-art <55270612+Vexati-art@users.noreply.github.com>
2020-12-01 15:13:03 +0800

    Create script.js

---
+++ Java/script.js
@@ -0,0 +1,294 @@
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+  
+      ctx.beginPath();
+      ctx.moveTo(195,20);
+      ctx.lineTo(300,100);
+      ctx.arc(195,170,0,5,0);
+      ctx.lineTo(100,100,);
+      
+      ctx.closePath();
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx. beginPath();
+      ctx. moveTo(20,30);
+      ctx.lineTo(120,110);
+      ctx.arc(190,100,10,0,0);
+      ctx.closePath();
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx. beginPath();
+      ctx. moveTo(370,30);
+      ctx.lineTo(200,100);
+      ctx.arc(260,109,20,0,0);
+      ctx.closePath();
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+  
+  
+     
+      ctx.beginPath();
+      ctx.moveTo(685,20);
+      ctx.lineTo(550,100);
+      ctx.arc(800,100,20,0,0);
+      
+      ctx.closePath();
+      ctx.fillStyle = "red";
+      ctx.fill();
+      
+
+      ctx.beginPath();
+      ctx.moveTo(685,40)
+      ctx.lineTo(550,100);
+      ctx.arc(800,100,20,0,0);
+     
+     
+      ctx.closePath();
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.moveTo(685,60)
+      ctx.lineTo(550,100);
+      ctx.arc(800,100,20,0,0);
+     
+     
+      ctx.closePath();
+      ctx.fillStyle = "green";
+      ctx.fill();
+
+
+      ctx.beginPath();
+      ctx.moveTo(1000,20);
+      ctx.lineTo(1000,150);
+      ctx.arc(1180,150,20,0,0);
+      ctx.lineTo(1200,20);
+      
+      ctx.closePath();
+      ctx.fillStyle = "red";
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.moveTo(1020,40);
+      ctx.lineTo(1020,130);
+      ctx.arc(1160,130,20,0,0);
+      ctx.lineTo(1180,40);
+      
+      ctx.closePath();
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.moveTo(1040,60);
+      ctx.lineTo(1040,110);
+      ctx.arc(1140,110,20,0,0);
+      ctx.lineTo(1160,60);
+      
+      ctx.closePath();
+      ctx.fillStyle = "green";
+      ctx.fill();
+
+
+      ctx.beginPath();
+      ctx.moveTo(100,370);
+      ctx.lineTo(300,370);
+      ctx.arc(300,350,0,5,0);
+      ctx.lineTo(100,350,);
+     
+      
+      ctx.closePath();
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+
+      ctx.beginPath();
+      ctx.moveTo(100,470);
+      ctx.lineTo(300,470);
+      ctx.arc(300,400,0,0,0);
+      ctx.lineTo(100,400,);
+     
+      
+      ctx.closePath();
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(100,520);
+      ctx.lineTo(300,520);
+      ctx.arc(300,500,0,0,0);
+      ctx.lineTo(100,500,);
+      
+      ctx.closePath();
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+
+
+      ctx.beginPath();
+      ctx.arc(740, 430, 70, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(570, 430, 70, 0, 2 * Math.PI);
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(660, 430, 70, 0, 2 * Math.PI);
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.fillStyle = "green";
+      ctx.beginPath();
+      ctx.moveTo(1130, 400.0);
+      ctx.lineTo(1210,415);
+      ctx.lineTo(1130, 440);
+      ctx.lineTo(1113,550);
+      
+
+      ctx.lineTo(1090,440);
+      ctx.lineTo(1010,415);
+      ctx.lineTo(1090,400);
+     
+
+      //top
+      ctx.lineTo(1110,300);
+     
+    
+    
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1110, 380, 10, 0, 2 * Math.PI);
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1110, 460, 10, 0, 2 * Math.PI);
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1150, 418, 10, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1072, 418, 10, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(1600,20);
+      ctx.lineTo(1350,100);
+      ctx.arc(1600,170,0,5,0);
+      ctx.lineTo(1420,100,);
+      
+      ctx.closePath();
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+
+      ctx.beginPath();
+      ctx.moveTo(1650,20);
+      ctx.lineTo(1440,100);
+      ctx.arc(1650,170,0,5,0);
+      ctx.lineTo(1490,100,);
+      
+      ctx.closePath();
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(1700,20);
+      ctx.lineTo(1505,100);
+      ctx.arc(1700,170,0,5,0);
+      ctx.lineTo(1550,100,);
+      
+      ctx.closePath();
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(1600,350);
+      ctx.lineTo(1600,500);
+      ctx.arc(1350,500,20,0,0);
+      ctx.lineTo(1370,350);
+      
+      ctx.closePath();
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(1485,350);
+      ctx.lineTo(1485,500);
+      ctx.arc(1460,500,20,0,0);
+      ctx.lineTo(1480,350);
+      
+      ctx.closePath();
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(1600,420);
+      ctx.lineTo(1600,425);
+      ctx.arc(1350,425,20,0,0);
+      ctx.lineTo(1370,420);
+      
+      ctx.closePath();
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1430, 385, 25, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1540, 385, 25, 0, 2 * Math.PI);
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1430, 460, 25, 0, 2 * Math.PI);
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(1540, 460, 25, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
+      ctx.fill();
+      ctx.stroke();
+
+    }
+  }
+
+ 
